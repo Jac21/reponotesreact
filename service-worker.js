2ee31cfff0efa3bd3e020af26aa476a13f65ce38
Jeremy Cantu <jcantu521@gmail.com>
2017-10-12 10:53:51 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","7a99d63d10fb2e826a2e6a061548c9dc"],["manifest.json","4025dad7a842920664c527ef90242dc7"],["static/css/main.25c21038.css","3ec729db1509ec97b69d3b273bf80787"],["static/js/main.24c5add2.js","632f97f44fa2e5f0a95af05eaa09a056"]];
+var precacheConfig = [["index.html","d17b5162755f0f43c8475841275eab0c"],["manifest.json","4025dad7a842920664c527ef90242dc7"],["static/css/main.25c21038.css","3ec729db1509ec97b69d3b273bf80787"],["static/js/main.25afb4e2.js","db416d57b7441125ad26d24d895ea632"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
